74cd2e17fd1643cf3a51bf9ac3db5ce93fa02a03
Josh <toyjtoy@gmail.com>
2016-04-16 21:26:48 -0400

    add additional pages

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   root 'blogposts#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get 'pages/about' => 'pages#about'
+  get 'pages/contact' => 'pages#contact'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
