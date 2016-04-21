5c40dbab00fec601abe6a992251dfa0dc5c7e872
Josh <toyjtoy@gmail.com>
2016-04-21 17:53:49 -0400

    fix typo on env files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
     port:                 587,
     domain:               'gmail.com',
     user_name:            ENV["EMAIL"],
-    password:             ENV["PASSWORD"]
+    password:             ENV["PASSWORD"],
     authentication:       'plain',
     enable_starttls_auto: true  }
 end
