e1f69056f651651ab74ff1b231e72dee7e7be15e
Josh <toyjtoy@gmail.com>
2016-05-25 19:37:30 -0400

    add asset path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def download
+    send_file "#{RAILS_ROOT}/assets/#{params[:JoshuaToyResume]}", :type=>"text/pdf"
+  end
 end
