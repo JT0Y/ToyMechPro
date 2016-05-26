e1b68bba0f3b6370c49abc32072e4410d0f2db44
Josh <toyjtoy@gmail.com>
2016-05-25 20:05:54 -0400

    add http auth

--- app/controllers/blogposts_controller.rb
+++ app/controllers/blogposts_controller.rb
@@ -1,4 +1,5 @@
 class BlogpostsController < ApplicationController
+  http_basic_authenticate_with name: "admin", password: "password", except: :index
   before_action :set_blogpost, only: [:show, :edit, :update, :destroy]
 
   # GET /blogposts
